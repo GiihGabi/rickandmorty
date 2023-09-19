f4abe831ce4aae7d3ca5040e166e4325d67487b3
GiihGabi <giovannacostabatista@gmail.com>
2023-09-19 20:51:07 -0300

    Subindo parte do projeto

--- VUE/src/App.vue
+++ VUE/src/App.vue
@@ -1,86 +1,81 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+// import { RouterLink, RouterView } from 'vue-router'
+import { ref } from "vue";
+
+let num = ref(0);
 
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <nav class="navbar">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon" id="teste"></span>
+        </button>
+        <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+          <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Dark offcanvas</h5>
+            <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+          <div class="offcanvas-body">
+            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Link</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Dropdown
+                </a>
+                <ul class="dropdown-menu dropdown-menu-dark">
+                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">Another action</a></li>
+                  <li>
+                    <hr class="dropdown-divider">
+                  </li>
+                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </li>
+            </ul>
+            <form class="d-flex mt-3" role="search">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-success" type="submit">Search</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </nav>
   </header>
 
-  <RouterView />
+  <div class="main">
+    <router-view></router-view>
+  </div>
+
+  <!-- <RouterView /> -->
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.navbar-toggler:focus {
+  box-shadow: none;
+}
+.navbar-toggler {
+  border: none;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+nav[data-v-7a7a37b1] {
+  height: 80px;
+  background-color: black;
+  box-shadow: 0px 0px 20px 0px #00F227;
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
--- VUE/src/views/HomeView.vue
+++ VUE/src/views/HomeView.vue
@@ -1,10 +1,70 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { onMounted, reactive, ref } from 'vue';
+// import ListPokemons from '../components/ListSW.vue';
+
+let baseUrlSvg = ref("https://raw.githubusercontent.com/PokeApi/sprites/master/sprites/pokemon/other/dream-world/");
+let pokemons = reactive(ref());
+
+onMounted(() => {
+  fetch("https://pokeapi.co/api/v2/pokemon/?limit=20&offset=0")
+  .then(response => response.json())
+  .then(response => {
+    pokemons.value = response.results;
+    console.log(response);
+  })
+})
 </script>
+
 
 <template>
   <main>
-    <!-- teste -->
-    <TheWelcome />
+    <div class="container">
+      <div id="container-esquerda">
+        <div class="container-esquerda">
+          <img id="logoA" src="../components/image/LogoA.png" alt="logoRickandMorty" width="397" height="115.72">
+          <div class="h1-div">
+            <h1 >
+              "Viver é  <p>arriscar</p> <p class="p-tudo">tudo"</p>
+            </h1>
+          </div>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+main {
+  width: 100vw;
+  height: 100vh;
+  background-color: black;
+  overflow: hidden;
+}
+
+#container-esquerda {
+  display: flex;
+  margin: 51px 0 0 80px;
+}
+
+.container-esquerda {
+  display: block;
+}
+
+.p-tudo {
+  color: #11B0C8;
+}
+
+.h1-div {
+  margin-top: 60px;
+}
+
+
+
+
+</style>
