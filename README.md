7988723721d71a2ff10b615712d8f30f1a9bed8b
ti.estagiario <ti.estagiario@TI-12525.UNIMAR>
2023-09-26 17:24:32 -0300

    Projeto finalizado

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
+#DOCUMENTAÇÃO
+## Para rodar o projeto
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Compile and Minify for Production
+
+```sh
+npm run build
+```
+
+#SOBRE
+Tenho 3 páginas
+- 1°Página: introdução
+- 2°Página: Episódios, onde mostra o dia de lançamento de cada episódio mas sem imagem
+- 3°Página: Mostra os personagens existentes, e dentro do modal fala a localização, usei duas APIs dentro de uma página
+
+
 ![rickaaaaa](https://github.com/GiihGabi/rickandmorty/assets/101181715/9686dc9e-3819-4695-98c6-d98ef896fd5e)
--- VUE/src/App.vue
+++ VUE/src/App.vue
@@ -18,35 +18,21 @@ let num = ref(0);
         </button>
         <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
           <div class="offcanvas-header">
-            <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Dark offcanvas</h5>
+            <p class="offcanvas-title" id="offcanvasDarkNavbarLabel">MENU</p>
             <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
           </div>
-          <div class="offcanvas-body">
-            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
-                </a>
-                <ul class="dropdown-menu dropdown-menu-dark">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
-                  <li>
-                    <hr class="dropdown-divider">
-                  </li>
-                  <li><a class="dropdown-item" href="#">Something else here</a></li>
-                </ul>
-              </li>
-            </ul>
-            <form class="d-flex mt-3" role="search">
-              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-              <button class="btn btn-success" type="submit">Search</button>
-            </form>
+          <div id="menu-conteudo">
+            <div>
+              <div>
+                <routerLink to="/" class="nav-link" aria-current="page">Home</routerLink>
+              </div>
+              <div>
+                <routerLink to="/episodio" class="nav-link" aria-current="page">Episódios</routerLink>
+              </div>
+              <div>
+                <routerLink to="/personagens" class="nav-link" aria-current="page">Personagens</routerLink>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -90,7 +76,35 @@ nav[data-v-7a7a37b1] {
 .navbar-toggler-icon {
   background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 30 30'%3e%3cpath stroke='rgba(123, 245, 30, 1)' stroke-linecap='round' stroke-miterlimit='10' stroke-width='2' d='M4 7h22M4 15h22M4 23h22'/%3e%3c/svg%3e");
 }
+#offcanvasDarkNavbar{
+  background-color: black !important;
+  border-radius: 0 0 0 8em;
+  border-left: 1px solid #00F227;
+  font-size: 26px;
+}
+.offcanvas-header {
+  justify-content: space-between;
+  margin: 2em 1em 0 1em;
+}
 
+#menu-conteudo {
+  font-size: 26px;
+  margin-top: 2em;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+.offcanvas-body {
+  color: white;
+}
 
-
+.nav-link:hover {
+  color: #00F227;
+}
+ #gif {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+ }
 </style>
